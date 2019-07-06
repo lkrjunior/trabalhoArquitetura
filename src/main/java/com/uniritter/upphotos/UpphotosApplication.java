c979f0b68fcde13fed6305ea771f114b96563ac8
lkrjunior <lkrjunior@terra.com.br>
2019-07-06 17:48:20 -0300

    Start project UpPhotos

---
+++ src/main/java/com/uniritter/upphotos/UpphotosApplication.java
@@ -0,0 +1,13 @@
+package com.uniritter.upphotos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpphotosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpphotosApplication.class, args);
+	}
+
+}
