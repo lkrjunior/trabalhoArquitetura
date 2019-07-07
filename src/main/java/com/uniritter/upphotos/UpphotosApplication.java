f2097a585ebc914a71a22fb414110c034e197640
lkrjunior <lkrjunior@terra.com.br>
2019-07-06 23:55:28 -0300

    Status page

--- src/main/java/com/uniritter/upphotos/UpphotosApplication.java
+++ src/main/java/com/uniritter/upphotos/UpphotosApplication.java
@@ -2,8 +2,10 @@ package com.uniritter.upphotos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "controller")
 public class UpphotosApplication {
 
 	public static void main(String[] args) {
