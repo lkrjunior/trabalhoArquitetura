735266b82ec35ab44769c8fb5d254486aa140cd2
lkrjunior <lkrjunior@terra.com.br>
2019-07-08 18:32:03 -0300

    Fix http methods and controller

--- src/main/java/bo/PhotoBO.java
+++ src/main/java/bo/PhotoBO.java
@@ -1,17 +1,34 @@
 package bo;
 
-import model.Photo;
+import model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 import repository.PhotoRepository;
-
 import java.io.IOException;
+import java.util.Base64;
+import java.util.List;
 
 @Component
 public class PhotoBO {
     @Autowired
     private PhotoRepository photoRepository;
+
+    public List<Photo> FixPhotos(List<Photo> photos)
+    {
+        for (Photo item : photos) {
+            item.setPhotoString("data:image/jpg;base64," + Base64.getEncoder().encodeToString(item.getPhoto()));
+        }
+        return photos;
+    }
+
+    public Photo CreatePhotoWithPerson(Person person)
+    {
+        Photo photo = new Photo();
+        photo.setPerson(person);
+
+        return photo;
+    }
 
     public void Save(Photo photo, MultipartFile file) throws IOException {
         photo.setPhoto(file.getBytes());
