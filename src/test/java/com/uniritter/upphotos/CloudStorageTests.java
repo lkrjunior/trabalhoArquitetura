fbbd650102be134ce5b76b80a241a1291612debd
lkrjunior <lkrjunior@terra.com.br>
2019-07-27 11:08:27 -0300

    Mock tests for CloudStorage

--- src/test/java/com/uniritter/upphotos/CloudStorageTests.java
+++ src/test/java/com/uniritter/upphotos/CloudStorageTests.java
@@ -1,35 +1,75 @@
 package com.uniritter.upphotos;
 
 import cloudstorage.DropboxCloudStorage;
+import com.dropbox.core.DbxDownloader;
+import com.dropbox.core.DbxException;
 import com.dropbox.core.v2.DbxClientV2;
+import com.dropbox.core.v2.files.DbxUserFilesRequests;
+import com.dropbox.core.v2.files.FileMetadata;
+import com.dropbox.core.v2.files.UploadBuilder;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(SpringRunner.class)
 public class CloudStorageTests
 {
     private String fileNameToTest = "filenamemock";
 
-    @Mock
     private DbxClientV2 client;
-    @Mock
+    private DbxUserFilesRequests files;
+    private UploadBuilder uploadBuilder;
+    private DbxDownloader<FileMetadata> fileDownload;
+    private DbxException dbxException;
+
+    private InputStream photoDownload;
     private FileInputStream photo;
-    @Mock
+    private FileMetadata fileMetadata;
+
     private DropboxCloudStorage dropboxCloudStorage;
+
+    @Before
+    public void Setup()
+    {
+        client = mock(DbxClientV2.class);
+        files = mock(DbxUserFilesRequests.class);
+        uploadBuilder = mock(UploadBuilder.class);
+        fileDownload = mock(DbxDownloader.class);
+        dbxException = mock(DbxException.class);
+
+        photoDownload = mock(InputStream.class);
+        photo = mock(FileInputStream.class);
+        fileMetadata = mock(FileMetadata.class);
+
+        dropboxCloudStorage = new DropboxCloudStorage();
+    }
 
     @Test
     public void DropboxUploadPhoto()
     {
         ///region Arrange
-        when(dropboxCloudStorage.UploadFile(client, photo, fileNameToTest)).thenReturn(true);
+        try
+        {
+            //void
+            //doNothing().when(client.files().uploadBuilder("/" + fileNameToTest).uploadAndFinish(photo));
+            when(client.files()).thenReturn(files);
+            when(files.uploadBuilder(any(String.class))).thenReturn(uploadBuilder);
+            when(uploadBuilder.uploadAndFinish(photo)).thenReturn(fileMetadata);
+        }
+        catch (DbxException e)
+        {
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
         ///endregion
 
         ///region Act
@@ -38,7 +78,6 @@ public class CloudStorageTests
 
         ///region Assert
         assertTrue(uploadPhotoSuccess);
-        verify(dropboxCloudStorage, times(1)).UploadFile(client, photo, fileNameToTest);
         ///endregion
     }
 
@@ -47,7 +86,17 @@ public class CloudStorageTests
     {
         ///region Arrange
         InputStream photoCloud;
-        when(dropboxCloudStorage.DownloadFile(client, fileNameToTest)).thenReturn(photo);
+
+        try
+        {
+            when(client.files()).thenReturn(files);
+            when(files.download(any(String.class))).thenReturn(fileDownload);
+            when(fileDownload.getInputStream()).thenReturn(photoDownload);
+        }
+        catch (DbxException e)
+        {
+            e.printStackTrace();
+        }
         ///endregion
 
         ///region Act
@@ -56,7 +105,33 @@ public class CloudStorageTests
 
         ///region Assert
         assertTrue(photoCloud != null);
-        verify(dropboxCloudStorage, times(1)).DownloadFile(client, fileNameToTest);
+        ///endregion
+    }
+
+    //@Test(expected = DbxException.class)
+    @Test
+    public void DropboxDownloadPhotoDbxException()
+    {
+        ///region Arrange
+        InputStream photoCloud;
+
+        try
+        {
+            when(client.files()).thenReturn(files);
+            when(files.download(any(String.class))).thenThrow(new DbxException("Error to download file"));
+        }
+        catch (DbxException e)
+        {
+            e.printStackTrace();
+        }
+        ///endregion
+
+        ///region Act
+        photoCloud = dropboxCloudStorage.DownloadFile(client, fileNameToTest);
+        ///endregion
+
+        ///region Assert
+        assert(photoCloud == null);
         ///endregion
     }
 }
